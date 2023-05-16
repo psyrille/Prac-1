c59a2920ed8e8924b341d975bc4fdc75587ac1ac
JakeTacogdoy <jaketacogdoy88@gmail.com>
2023-05-16 22:32:06 +0800

    nag add ug css sa index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,22 +2,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Tae ina</title>
+    <title>SIMPLE CRUD</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
     <style>
+        .container{
+            height: 100vh;
+        }
         body{
-            background-color: red
-        }   
+            font-family: 'Montserrat', sans-serif;
+        }
     </style>
 </head>
 <body>
-    <div class="container mt-2">
+    <div class="container mt-2 d-flex flex-column align-items-center justify-content-center">
         <div class="row">
-            <div class="col-lg-12 margin-tb">
+            <div class="col-lg-12 margin-tb d-flex flex-column align-items-center mb-3">
                 <div class="pull-left">
-                    <h2>Laravel 9 CRUD Example Tutorial</h2>
+                    <h2><b>SIMPLE CRUD SYSTEM</b></h2>
                 </div>
-                <div class="pull-right mb-2">
+                <div class="pull-right mb-2 mt-3">
                     <a class="btn btn-success" href="{{ route('companies.create') }}"> Create Company</a>
                 </div>
             </div>
@@ -27,9 +33,9 @@
                 <p>{{ $message }}</p>
             </div>
         @endif
-        <table class="table table-bordered">
+        <table class="table table-bordered table-striped mt-5">
             <thead>
-                <tr>
+                <tr class="table-primary">
                     <th>S.No</th>
                     <th>Company Name</th>
                     <th>Company Email</th>
